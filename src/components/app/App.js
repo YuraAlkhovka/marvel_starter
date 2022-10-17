9ae76a7e06967856c68d66220b469d463f81bb4e
Yura Alhovka <ualhovka@gmail.com>
2022-10-17 21:23:49 +0300

    marvel service

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react/cjs/react.production.min";
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,14 +7,17 @@ import CharInfo from "../charInfo/CharInfo";
 import decoration from '../../resources/img/vision.png';
 
 class App extends Component {
+    
     state = {
         selectedChar: null
     }
+
     onCharSelected = (id) => {
         this.setState({
             selectedChar: id
         })
     }
+
     render() {
         return (
             <div className="app">
@@ -22,8 +25,8 @@ class App extends Component {
                 <main>
                     <RandomChar/>
                     <div className="char__content">
-                        <CharList onCharSelected = {this.onCharSelected}/>
-                        <CharInfo charId = {this.state.selectedChar}/>
+                        <CharList onCharSelected={this.onCharSelected}/>
+                        <CharInfo charId={this.state.selectedChar}/>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -5,18 +5,19 @@ import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
 class CharList extends Component {
+
     state = {
         charList: [],
         loading: true,
         error: false
     }
-
+    
     marvelService = new MarvelService();
 
     componentDidMount() {
         this.marvelService.getAllCharacters()
-        .then(this.onCharListLoaded)
-        .catch(this.onError)
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
     }
 
     onCharListLoaded = (charList) => {
@@ -33,29 +34,31 @@ class CharList extends Component {
         })
     }
 
+    // Этот метод создан для оптимизации, 
+    // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
         const items =  arr.map((item) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
-    }
-
-    return (
-        <li
-        className='char__item'
-        key={item.id}
-        onClick = {() => this.props.onCharSelected(item.id)}>
-            <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-            <div className="char__name">{item.name}</div>
-        </li>
-    )
-});
-
-    return (
-        <ul className="char__grid">
-            {items}
-        </ul>
-    )
+            }
+            
+            return (
+                <li 
+                    className="char__item"
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
+                </li>
+            )
+        });
+        // А эта конструкция вынесена для центровки спиннера/ошибки
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
     }
 
     render() {
