d50af6187963ebc5d1dfc6882e9a67797398a487
Yura Alhovka <ualhovka@gmail.com>
2022-10-17 23:35:08 +0300

    Load More in CharList

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=bcaaf89f5b8e43f6233c3bba964010e2';
+    _baseOffSet = 210;
     getResource = async (url) => {
         let res = await fetch(url);
     
@@ -11,8 +12,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffSet) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
