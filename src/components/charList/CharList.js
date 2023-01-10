469a97270a49346572c14fcea31cd814c2f12a9c
Yura Alhovka <ualhovka@gmail.com>
2023-01-10 20:11:36 +0300

    React Transition Group

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,6 @@
 import {useState, useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -56,7 +57,8 @@ const CharList = (props) => {
             }
             
             return (
-                <li 
+                <CSSTransition key={item.id} timeout={500} classNames="char__item">
+                    <li 
                     className="char__item"
                     tabIndex={0}
                     ref={el => itemRefs.current[i] = el}
@@ -65,7 +67,7 @@ const CharList = (props) => {
                         props.onCharSelected(item.id);
                         focusOnItem(i);
                     }}
-                    onKeyPress={(e) => {
+                    onKeyDown={(e) => {
                         if (e.key === ' ' || e.key === "Enter") {
                             props.onCharSelected(item.id);
                             focusOnItem(i);
@@ -74,12 +76,15 @@ const CharList = (props) => {
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
+                </CSSTransition>
             )
         });
         // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
             <ul className="char__grid">
-                {items}
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
             </ul>
         )
     }
